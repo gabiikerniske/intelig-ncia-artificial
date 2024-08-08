4c6da34ac7bda5de3640f32dee2456eadf112497
gabiikerniske <112017069+gabiikerniske@users.noreply.github.com>
2024-08-08 08:26:23 -0300

    0808

--- script.js
+++ script.js
@@ -9,18 +9,12 @@ const perguntas = [
         enunciado: "Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?",
         alternativas: [
         {
-            texto:"Isso é assustador!",
-            afirmação: [ 
-                "No início, ficou com medo do que essa tecnologia pode fazer.",
-            "Achou assustador pensar na velocidade com que a tecnologia está avançando."
-            ]
+            texto: "Isso é assustador!",
+            afirmacao: "No início, ficou com medo do que essa tecnologia pode fazer."
         },
         {
-            texto:"Isso é maravilhoso!",
-            afirmação: [
-                "Quis saber como usar IA no seu dia a dia.",
-            "Pensou que a IA pode ajudar em tarefas da sua vida."
-            ]
+            texto: "Isso é maravilhoso!",
+            afirmacao: "Quis saber como usar IA no seu dia a dia."
         }
         ],
     },
@@ -28,25 +22,25 @@ const perguntas = [
         enunciado: "Com a descoberta desta tecnologia uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre IA. No fim de uma aula ela pede que Gabriel escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude Gabriel toma?",
         alternativas: [
            {
-            texto:"Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.", 
-            afirmação: "Afirmação 1"
+            texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.", 
+            afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
            },
     {
-               texto:"Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-            afirmação: "Afirmação 2"
+               texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+            afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
         }
     ]
 },
     {
-        enunciado: "Depois que Gabriel escreveu o trabalho, teve uma discussão sobre o impacto da IA no trabalho do futuro o que Gabriel faz:",
+        enunciado: "Depois que Gabriel escreveu o trabalho, teve uma discussão sobre o impacto da IA no trabalho do futuro o que Gabriel faz?",
         alternativa: [
             {
             texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas", 
-            afirmação: "Afirmação 1"
+            afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA"
             },
             {
             texto:"Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-            afirmação: "Afirmação 2"
+            afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
             }
         ],
     },
@@ -55,15 +49,29 @@ const perguntas = [
         alternativas:[
             {
             texto: "Criar uma imagem utilizando um gerador de imagem de IA.", 
-            afirmação: "Afirmação 1"
+            afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
             }
             {
             texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            afirmação:"Afirmação 2"    
+            afirmacao:"Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."    
             }
         ],
     },
+    {
+        enunciado: "Gabriel tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que Gabriel faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+            }
+        ]
+    },
 ];
+
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
@@ -78,28 +86,29 @@ function mostraPergunta(){
     caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
+
 function mostraAlternativas(){
     for (const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () =>
-            respostaSelecionada(alternativa)); 
-            function respostaSelecionada (opcaoSelecionada){
-                const afirmacoes = opcaoSelecionada.afirmação;
-                historiaFinal += afirmacoes + "";
-                atual++;
-                mostraPergunta();
-            };
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa)); 
         caixaAlternativas.appendChild(botaoAlternativas);
         }
+}
+
+function respostaSelecionada (opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + "";
+    atual++;
+    mostraPergunta();
 }
 function mostraResultado (){
     caixaPerguntas.textContent = "Em 2049 ...";
     textoResultado. textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
-mostraPergunta();
 
+mostraPergunta();
 const lapis = {
   tamanho: 20,
   tipo: 'HB',
